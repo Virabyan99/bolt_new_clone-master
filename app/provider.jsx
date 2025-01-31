36519e5ed0f6980f0b91221d69db3f4bcb64df24
Virabyan99 <gmparstone99@gmail.com>
2025-02-01 02:20:04 +0400

    Payment pay pal added

--- app/provider.jsx
+++ app/provider.jsx
@@ -4,6 +4,7 @@ import { SidebarProvider } from '@/components/ui/sidebar'
 import { MessagesContext } from '@/context/MessagesContext'
 import { UserDetailContext } from '@/context/UserDetailContext'
 import { api } from '@/convex/_generated/api'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { useConvex } from 'convex/react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
@@ -33,20 +34,23 @@ function Provider({ children }) {
     <div>
       <GoogleOAuthProvider
         clientId={process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID_KEY}>
-        <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
-          <MessagesContext.Provider value={{ messages, setMessages }}>
-            <NextThemesProvider
-              attribute="class"
-              defaultTheme="dark"
-              enableSystem
-              disableTransitionOnChange>
-              <SidebarProvider defaultOpen={false}>
-                <AppSidebar />
-                {children}
-              </SidebarProvider>
-            </NextThemesProvider>
-          </MessagesContext.Provider>
-        </UserDetailContext.Provider>
+        <PayPalScriptProvider
+          options={{ clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID }}>
+          <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+            <MessagesContext.Provider value={{ messages, setMessages }}>
+              <NextThemesProvider
+                attribute="class"
+                defaultTheme="dark"
+                enableSystem
+                disableTransitionOnChange>
+                <SidebarProvider defaultOpen={false}>
+                  <AppSidebar />
+                  {children}
+                </SidebarProvider>
+              </NextThemesProvider>
+            </MessagesContext.Provider>
+          </UserDetailContext.Provider>
+        </PayPalScriptProvider>
       </GoogleOAuthProvider>
     </div>
   )
--- components/custom/PricingModel.jsx
+++ components/custom/PricingModel.jsx
@@ -1,17 +1,55 @@
-import Lookup from "@/data/Lookup"
-import { Button } from "../ui/button"
+import Lookup from '@/data/Lookup'
+import { Button } from '../ui/button'
+import { PayPalButtons } from '@paypal/react-paypal-js'
+import { useContext, useEffect, useState } from 'react'
+import { UserDetailContext } from '@/context/UserDetailContext'
+import { useMutation } from 'convex/react'
+import { api } from '@/convex/_generated/api'
 
 const PricingModel = () => {
+  const { userDetail, setUserDetail } = useContext(UserDetailContext)
+  const UpdateToken = useMutation(api.users.UpdateToken)
+  const [selectedOption, setSelectedOption] = useState()
+
+  const onPaymentSuccess = async () => {
+    const token = userDetail?.token + Number(selectedOption?.value)
+    console.log(token)
+    await UpdateToken({
+      token: token,
+      userId: userDetail?._id,
+    })
+  }
   return (
     <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
       {Lookup.PRICING_OPTIONS.map((pricing, index) => (
-          <div className="border p-5  rounded-xl flex flex-col gap-3" key={index}>
-            <h2 className="font-bold text-2xl">{pricing.name}</h2>
-            <h2 className="font-medium text-lg">{pricing.tokens} Tokens</h2>
-            <p className="text-gray-400">{pricing.desc}</p>
-            <h2 className="font-bold text-4xl text-center mt-6">${pricing.price}</h2>
-            <Button className="w-full my-auto">Upgrade to {pricing.name}</Button>
-          </div>
+        <div className="border p-5  rounded-xl flex flex-col gap-3" key={index}>
+          <h2 className="font-bold text-2xl">{pricing.name}</h2>
+          <h2 className="font-medium text-lg">{pricing.tokens} Tokens</h2>
+          <p className="text-gray-400">{pricing.desc}</p>
+          <h2 className="font-bold text-4xl text-center mt-6">
+            ${pricing.price}
+          </h2>
+          {/* <Button className="w-full my-auto">Upgrade to {pricing.name}</Button> */}
+          <PayPalButtons
+            onClick={() => setSelectedOption(pricing)}
+            disabled={!userDetail}
+            style={{ layout: 'horizontal' }}
+            onCancel={() => console.log('cancelled')}
+            onApprove={() => onPaymentSuccess()}
+            createOrder={(data, actions) => {
+              return actions.order.create({
+                purchase_units: [
+                  {
+                    amount: {
+                      value: pricing.price,
+                      currency_code: 'USD',
+                    },
+                  },
+                ],
+              })
+            }}
+          />
+        </div>
       ))}
     </div>
   )
